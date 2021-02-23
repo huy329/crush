41bee3345dd758c35395a1907ecae8c86414db42
Huy <73300114+huy329@users.noreply.github.com>
2021-02-24 00:44:49 +0700

    Create heart.cpp

---
+++ heart.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string name;
+  int birth_year;
+  
+  cout << "Crush's Name: ";
+  getline(cin, name);
+  
+  cout << "Crush's Birth Year: ";
+  cin >> birth_year;
+  
+  cout << "*****************************" << endl;
+  cout << "***     0 0 0   0 0 0     ***" << endl;
+  cout << "***   0 0 0 0 0 0 0 0 0   ***" << endl;
+  cout << "*** 0 0 0 0 0 0 0 0 0 0 0 ***" << endl;
+  cout << "*** 0 0 0 0 0 0 0 0 0 0 0 ***" << endl;
+  cout << "***   0 0 0 0 0 0 0 0 0   ***" << endl;
+  cout << "***     0 0 0 0 0 0 0     ***" << endl;
+  cout << "***       0 0 0 0 0       ***" << endl;
+  cout << "***         0 0 0         ***" << endl;
+  cout << "***           0           ***" << endl;
+  cout << "*****************************" << endl;
+  
+  return 0;
+}
