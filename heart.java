12c17bda026b69c59d2555eb5eb3c0e6046b6e30
Huy <73300114+huy329@users.noreply.github.com>
2021-02-24 00:52:35 +0700

    Update heart.java

--- heart.java
+++ heart.java
@@ -1,18 +1,29 @@
 import java.lang.*;
 import java.util.Scanner;
 
-public class lover {
+public class huy {
 	public static void main(String[]args) {
-	System.out.println("*****************************");
-        System.out.println("***     0 0 0   0 0 0     ***");
-        System.out.println("***   0 0 0 0 0 0 0 0 0   ***");
-        System.out.println("*** 0 0 0 0 0 0 0 0 0 0 0 ***");
-        System.out.println("*** 0 0 0 0 0 0 0 0 0 0 0 ***");
-        System.out.println("***   0 0 0 0 0 0 0 0 0   ***");
-        System.out.println("***     0 0 0 0 0 0 0     ***");
-        System.out.println("***       0 0 0 0 0       ***");
-        System.out.println("***         0 0 0         ***");
-        System.out.println("***           0           ***");
-        System.out.println("*****************************");
+		Scanner sc = new Scanner(System.in);
+		
+		String name;
+		int birth_year;
+		
+		System.out.print("Crush's Name: ");
+		name = sc.nextLine();
+		
+		System.out.print("Crush's Birth Year: ");
+		birth_year = sc.nextInt();
+		
+		System.out.println("*****************************");
+		System.out.println("***     0 0 0   0 0 0     ***");
+		System.out.println("***   0 0 0 0 0 0 0 0 0   ***");
+		System.out.println("*** 0 0 0 0 0 0 0 0 0 0 0 ***");
+		System.out.println("*** 0 0 0 0 0 0 0 0 0 0 0 ***");
+		System.out.println("***   0 0 0 0 0 0 0 0 0   ***");
+		System.out.println("***     0 0 0 0 0 0 0     ***");
+		System.out.println("***       0 0 0 0 0       ***");
+		System.out.println("***         0 0 0         ***");
+		System.out.println("***           0           ***");
+		System.out.println("*****************************");
 	}
 }
