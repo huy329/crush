05134ed4557f90c0d22f0756327d1039be164c5b
Huy <73300114+huy329@users.noreply.github.com>
2021-02-24 00:16:00 +0700

    Update heart.java

--- heart.java
+++ heart.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class lover {
 	public static void main(String[]args) {
-		System.out.println("*****************************");
+	System.out.println("*****************************");
         System.out.println("***     0 0 0   0 0 0     ***");
         System.out.println("***   0 0 0 0 0 0 0 0 0   ***");
         System.out.println("*** 0 0 0 0 0 0 0 0 0 0 0 ***");
