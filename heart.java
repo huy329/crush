9430cdb2650b53cd859ca0fa6d7ec678d03e6b0d
Huy <73300114+huy329@users.noreply.github.com>
2021-02-24 00:15:32 +0700

    Add files via upload

---
+++ heart.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class lover {
+	public static void main(String[]args) {
+		System.out.println("*****************************");
+        System.out.println("***     0 0 0   0 0 0     ***");
+        System.out.println("***   0 0 0 0 0 0 0 0 0   ***");
+        System.out.println("*** 0 0 0 0 0 0 0 0 0 0 0 ***");
+        System.out.println("*** 0 0 0 0 0 0 0 0 0 0 0 ***");
+        System.out.println("***   0 0 0 0 0 0 0 0 0   ***");
+        System.out.println("***     0 0 0 0 0 0 0     ***");
+        System.out.println("***       0 0 0 0 0       ***");
+        System.out.println("***         0 0 0         ***");
+        System.out.println("***           0           ***");
+        System.out.println("*****************************");
+	}
+}
